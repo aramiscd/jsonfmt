9d7cc5e0618ae5e59b9ea6d22a82cc5fecc701b8
Aramís  Concepción Durán <aramiscd@gmail.com>
2019-09-30 08:26:23 +0200

    README.md

--- README.md
+++ README.md
@@ -1,132 +1,47 @@
 # jsonfmt
 
-A simple JSON pretty-printer, inspired by
-[elm-format](https://github.com/avh4/elm-format).
+A simple JSON pretty printer, inspired by
+[elm-format](https://github.com/avh4/elm-format), written
+in Haskell.
 
-Distinguishing features:
+It reads a JSON document from the standard input, reformats
+it and writes it to the standard output.  It has no
+configuration or command line options.
 
-* Arrays and objects are formatted like this:
+It will turn this:
 
-  ```json
-  [ "element_1"
-  , "element_2"
-  , "element_3"
-  ]
-  
-  { "key_1" : "value_1"
-  , "key_2" : "value_2"
-  , "key_3" : "value_3"
-  }
-  ```
+```json
+{"k1":"v1","k2":"v2","k3":["e1","e2","e3"]}
+```
     
-  instead of this:
+into this:
 
-  ```json
-  [ "element_1",
-    "element_2",
-    "element_3"
-  ]
-  
-  { "key_1" : "value_1",
-    "key_2" : "value_2",
-    "key_3" : "value_3"
-  }
-  ```
-
-* Input whitespace isn't considered.  No matter how you
-  wrap your JSON, the output stays the same.
-
-* No command line options and flags.
-  (I should perhaps add `-h, --help`.)
-
-* It's written in Haskell.
-
-----
-
-The prefix-comma style for array elements and object
-members is taken from [Elm](https://elm-lang.org).  I like
-this style a lot.  It is diff-friendly and makes commas
-more visible.
-
-Also, after a long period of failure and despair in this
-matter, I finally figured out how to write parsers, which
-brings my yak-shaving skills to the next level.  The first
-touchstone that came to mind was to write a JSON parser
-and pretty-printer.
-
-
-## Editor Integration
-
-A great property of Vim-like text editors is that you don't
-actually need plugins for things like code formatting.
-Any program that reads from stdin and prints to stdout can
-be invoked to operate on the contents of a text buffer.
-Editor "integration" is merely a matter of binding this
-action to a key.  I like to bind code formatters to the
-tab key.
-
-
-### Neovim
-
-Most of this is trying to keep the cursor position intact
-(because Vim doesn't on its own).  The actual binding is
-done by `nnoremap`.
-
+```json
+{ "k1" : "v1"
+, "k2" : "v2"
+, "k3" :
+    [ "e1"
+    , "e2"
+    , "e3"
+    ]
+}
 ```
-" ~/.config/nvim/after/ftplugin/json.vim
 
-function! Fmt()
-  execute "normal! mz"
-  try
-    undojoin
-    silent execute "%!jsonfmt"
-  catch /^Vim\%((\a\+)\)\=:E790/
-  endtry
-  try
-    execute "normal! `z"
-  catch /E20:/
-    execute "normal! G"
-  endtry
-endfunction
 
-nnoremap <silent> <tab> :call Fmt()<cr>
-setlocal shiftwidth=2
-setlocal tabstop=2
-```
+## Installation
+
+Download jsonfmt and put it in your `$PATH`.
+
+
+## Usage
+
+### Vim/Neovim
+
+Type `:%!jsonfmt` and hit `return` to format a JSON
+document.
 
 
 ### Kakoune
 
-Putting the following snippet in your `kakrc` allows
-you to set filetype-specific options in a file under
-`~/.config/kak/filetype/`:
-
-```
-# ~/.config/kak/kakrc
-
-hook global WinSetOption filetype=.* %{
-  try %{ source %sh{
-    echo ~/.config/kak/filetype/${kak_opt_filetype}.kak
-}}}
-```
-
-Haskell options go into `haskell.kak`, JavaScript options
-go into `javascript.kak`, and so on.  To see what filetype
-is associated with the current buffer, do this:
-
-```
-:echo %opt{filetype}
-```
-
-With the above snippet in your `kakrc` you
-can set your json formatter to `jsonfmt` in
-`~/.config/kak/filetype/json.kak`:
-
-```
-# ~/.config/kak/filetype/json.kak
-
-set window formatcmd 'jsonfmt'
-```
-
-As long as you stick to the `window` scope, these settings
-won't affect other buffers and filetypes.
+Type `%|jsonfmt` and hit `return` to format a JSON
+document.
--- src/IO.hs
+++ src/IO.hs
@@ -1,32 +1,19 @@
 module IO
 
 {-
-    `System.IO` is full of functions that might throw
-    exceptions at runtime.  For example, `hGetContents`
-    blows up when it gets bad data, `hPutStr` blows up
-    when it is applied to a closed handle.
-
-    I want these failures to be handled at the type
-    level.  Hence this wrapper module!  It provides safe
-    alternatives to some potentially failing `IO` functions
-    in `System.IO`.
-
-    I'm also renaming some things to get rid of single-
-    letter prefixes.  `hGetContents` should really be
-    named `getContents`, and the current `getContents`
-    should be written as `getContents stdin`.
-
-    This module just contains what I need.  It isn't an
-    exhaustive alternative to `System.IO`.
+    I/O streams, I/O access, I/O functions.
 -}
 
-( Mode ( Read , Write , Append , ReadWrite )
+( Mode
+    ( Read , Write , Append , ReadWrite )
 , stdin
 , stdout
 , stderr
+
 , print
-, println
+, printLine
 , printErr
+
 , putStr
 , putStrLn
 , getContents
@@ -48,7 +35,7 @@ import System.IO                ( Handle )
 
 data Mode
 {-
-    File access modes.
+    I/O stream access modes.
 -}
     = Read
     | Write
@@ -71,22 +58,13 @@ toIOMode mode =
 try :: IO a -> IO ( Result SomeException a )
 {-
     Try to perform an IO action.
-
-    This is just like `Control.Exception.try` but it
-    returns a `Result` (`Ok` or `Err`) instead of an
-    `Either` (`Right` or `Left`).
-
-    I don't like `Either` because `Left` vs. `Right`
-    isn't semantically helpful.  In most cases it's just
-    confusing because you never know what is left and what
-    is right.
 -}
 try action =
     Control.Exception.Safe.tryAny action
     |> andThen
         ( \ case
-            Left exception  -> return ( Err exception )
-            Right contents  -> return ( Ok contents )
+            Left error  -> return ( Err error )
+            Right value -> return ( Ok value )
         )
 
 
@@ -124,8 +102,8 @@ print string =
 
 println :: String -> IO ()
 {-
-    Write to the standard output stream, appending
-    a newline.
+    Append a newline and write to the standard output
+    stream.
 -}
 println string =
     System.IO.putStrLn string
@@ -133,8 +111,8 @@ println string =
 
 printErr :: String -> IO ()
 {-
-    Write to the standard error output stream, appending
-    a newline.
+    Append a newline and write to the standard error
+    output stream.
 -}
 printErr string =
     System.IO.hPutStrLn stderr string
@@ -142,13 +120,7 @@ printErr string =
 
 putStr :: Handle -> String -> IO ( Result SomeException () )
 {-
-    Write to a stream via its handle.
-
-    This might fail with an `IOException`, for example when
-    the stream has been closed already.  This function
-    forces the programmer to handle the exception, while
-    `System.IO.hPutStr` will blow up at runtime in this
-    case.
+    Write to a stream.
 -}
 putStr handle string =
     try ( System.IO.hPutStr handle string )
@@ -156,13 +128,7 @@ putStr handle string =
 
 putStrLn :: Handle -> String -> IO ( Result SomeException () )
 {-
-    Append a newline and write to a stream via its handle.
-
-    This might fail with an `IOException`, for example when
-    the stream has been closed already.  This function
-    forces the programmer to handle the exception, while
-    `System.IO.hPutStrLn` will blow up at runtime in
-    this case.
+    Append a newline and write to a stream.
 -}
 putStrLn handle string =
     try ( System.IO.hPutStrLn handle string )
@@ -170,13 +136,7 @@ putStrLn handle string =
 
 getContents :: Handle -> IO ( Result SomeException String )
 {-
-    Get the contents of an input stream via its handle.
-
-    This might fail with an `IOException`, for example
-    when the stream contains bad data.  This function
-    forces the programmer to handle the exception, while
-    `System.IO.hGetContents` will blow up at runtime in
-    this case.
+    Get data from a stream.
 -}
 getContents handle =
     try ( System.IO.Strict.hGetContents handle )
@@ -184,12 +144,7 @@ getContents handle =
 
 openFile :: Mode -> String -> IO ( Result SomeException Handle )
 {-
-    Open a file in the given access mode.
-
-    This might fail, for example when the file doesn't
-    exist.  This function forces the programmer to handle
-    the exception, while `System.IO.openFile` will blow
-    up at runtime in this case.
+    Open a file.
 -}
 openFile mode path =
     try ( System.IO.openFile path ( toIOMode mode ) )
