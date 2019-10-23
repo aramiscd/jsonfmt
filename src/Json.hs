0b5f2abb059b06beaaabc115c1c90bb5a2298a16
Aramís  Concepción Durán <aramiscd@gmail.com>
2019-10-23 17:55:06 +0200

    add license information

--- src/Json.hs
+++ src/Json.hs
@@ -1,6 +1,30 @@
 module Json
 
 {-
+    Copyright 2019, Aramis Concepcion Duran
+    
+    This file is part of jsonfmt.
+
+    Ulme is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation,
+    either version 3 of the License, or (at your option)
+    any later version.
+
+    Ulme is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR
+    A PARTICULAR PURPOSE.  See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General
+    Public License along with Foobar.  If not, see
+    <https://www.gnu.org/licenses/>.
+
+
+    -------------------------------------------------------
+
+
     This module provides the `Json` data type: a simple,
     well-typed Haskell-representation of JSON documents.
 -}
