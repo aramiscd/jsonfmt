3c512aa6a1751991757573e05151372f905bfa37
Aramís  Concepción Durán <aramiscd@gmail.com>
2019-10-23 19:39:02 +0200

    update license information

--- src/Parse.hs
+++ src/Parse.hs
@@ -5,13 +5,13 @@ module Parse
     
     This file is part of jsonfmt.
 
-    Ulme is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public
-    License as published by the Free Software Foundation,
-    either version 3 of the License, or (at your option)
-    any later version.
+    Jsonfmt is free software: you can redistribute it
+    and/or modify it under the terms of the GNU General
+    Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at
+    your option) any later version.
 
-    Ulme is distributed in the hope that it will be
+    Jsonfmt is distributed in the hope that it will be
     useful, but WITHOUT ANY WARRANTY; without even the
     implied warranty of MERCHANTABILITY or FITNESS FOR
     A PARTICULAR PURPOSE.  See the GNU General Public
