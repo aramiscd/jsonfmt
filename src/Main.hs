f750e5a23e91697244d2b7e168f34543fa6ad95d
Aramís Concepción Durán <aramis.duran@increaseyourskills.com>
2021-11-02 05:46:20 +0100

    Join up with ulme-json.

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,12 @@
-module Main
-
 {-
     Read a JSON document from stdin, parse it into a
     `Json` value, apply a pretty-printer to it and write
     the resulting string to the standard output.
 
-
     ----
 
     Copyright 2019-2020, Aramis Concepcion Duran
-    
+
     This file is part of jsonfmt.
 
     Jsonfmt is free software: you can redistribute it
@@ -29,47 +26,23 @@ module Main
     <https://www.gnu.org/licenses/>.
 -}
 
-( main
-)
-where
-
+module Main (main) where
 
 import Ulme
 
-import System.Exit      ( ExitCode ( ExitFailure ) , exitWith )
-import Ulme.IO          qualified as IO
-import Ulme.Json.Parse  qualified as Parse
+import System.Exit (ExitCode (ExitFailure), exitWith)
+import Ulme.IO qualified as IO
+import Ulme.Json.Parse qualified as Parse
 import Ulme.Json.Pretty qualified as Pretty
-import Ulme.String      qualified as String
 
 
 main :: IO ()
 main =
-    IO.getContents IO.stdin >>= \ case
-    Ok contents ->
-        case Parse.json contents of
-        Ok ( _ , json , [] ) ->
-            IO.println ( Pretty.print json )
-
-        Ok ( _ , _ , pending ) -> do
-            IO.printErr ( "Invalid JSON: " ++ pending )
-            exitWith ( ExitFailure 1 )
-
-        Err parseErrors -> do
-            printParseErrors parseErrors
-            exitWith ( ExitFailure 1 )
-
-    Err ioException -> do
-        IO.printErr ( show ioException )
-        exitWith ( ExitFailure 1 )
-
-
-printParseErrors :: List ( ( Natural , Natural ) , String ) -> IO ()
-printParseErrors errors =
-    let
-        printError ( ( line , column ) , error ) =
-            "(" ++  String.fromIntegral line ++
-            "," ++ String.fromIntegral column ++
-            "): " ++ error
-    in
-        IO.printErr ( String.join "\n" ( map printError errors ) )
+    IO.getContents IO.stdin >>= \case
+        Ok contents ->
+            case Parse.json contents of
+                Just json -> IO.println (Pretty.print json)
+                Nothing -> exitWith (ExitFailure 1)
+        Err ioException -> do
+            IO.printErr (show ioException)
+            exitWith (ExitFailure 1)
