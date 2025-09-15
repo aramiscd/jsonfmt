ad52a0a46458af11637c3993d18e596e26a4fbef
Aramís Concepción Durán <aramis@systemli.org>
2025-09-15 14:26:03 +0200

    Stelle die Ausgabe des Ergebnisses wieder her

--- scripts/build-dev.fish
+++ scripts/build-dev.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
 npx gren make Main --output=jsonfmt
+and chmod +x jsonfmt
--- scripts/build.fish
+++ scripts/build.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
 npx gren make Main --optimize --output=jsonfmt
+and chmod +x jsonfmt
